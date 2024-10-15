e714aad857802fd094b2da8e6d84a18e733285ea
Percival <emperorsheevpalpatine0@gmail.com>
2024-10-15 21:04:42 +0800

    style(FE-servant>order): update order showcasing

--- linkup-vue-servant/pages/order/order.vue
+++ linkup-vue-servant/pages/order/order.vue
@@ -23,43 +23,43 @@
         @scrolltoupper="reload"
         @scrolltolower="onReachBottom"
     >
-        <div
-            class="app-container"
-            v-for="order in orderList"
-            :key="order.id"
-            @click="orderDetailRedirect(order.id)"
-        >
-            <div class="order-content">
-                <div style="width: 100%;">
-                    <div style="display: flex; align-items: center;">
-                        <app-title bold="true" type="h3" style="width: 330px;">
-                            {{ language != "zh-Hans" ? order.title : order.titleCn }}
-                        </app-title>
-                    </div>
-                    <div class="order-detail">
-                        <div style="display:flex;justify-content: space-between;">
-                            <div v-if="order.status == 0" class="flex">
-                                <span class="status-dot yellow-dot"></span>
-                                <div class="status-text">{{ $t('profile>order.pending') }}</div>
-                            </div>
-                            <div v-if="order.status == 1 && isServiceInProgressState(order)" class="flex">
-                                <span class="status-dot green-dot"></span>
-                                <div class="status-text">{{ $t('profile>order.processing') }}</div>
-                            </div>
-                            <div v-if="order.status == 1 && !isServiceInProgressState(order)" class="flex">
-                                <span class="status-dot green-dot"></span>
-                                <div class="status-text">{{ $t('profile>order.confirmed') }}</div>
-                            </div>
-                            <div v-if="order.status == 2" class="flex">
-                                <span class="status-dot gray-dot"></span>
-                                <div class="status-text">{{ $t('profile>order.completed') }}</div>
-                            </div>
-                            <div v-if="order.status == 3" class="flex">
-                                <div class="status-dot red-dot"></div>
-                                <div class="status-text">{{ $t('profile>order.canceled') }}</div>
-                            </div>
-                        </div>
-                    </div>
+        <div class="app-container" v-for="order in orderList" :key="order.id" @click="orderDetailRedirect(order.id)" style="display: flex;flex-direction: column;align-items: center">
+            <div class="order-schedule">
+                <div>{{ language != "zh-Hans" ? 'Service Schedule: ' : "服务时间" }}</div>
+                <div>
+                    {{ $common.stampToTime(order.serviceScheduleStart, {yyyy: false, ss: false}) }}
+                    -
+                    {{ $common.stampToTime(order.serviceScheduleEnd, {yyyy: false, ss: false, MM: false, dd: false}) }}
+                </div>
+            </div>
+            <div class="service-type-price">
+                <app-title bold="true" type="h3">{{ language != "zh-Hans" ? order.serviceTypeName + ' Service' : order.serviceTypeNameCn + "服务" }}</app-title>
+                <app-title type="h3">¥{{ order.price }}</app-title>
+            </div>
+            <div class="order-address">
+                <div>{{order.address.addressName}}</div>
+                <div>{{order.address.detail}}</div>
+            </div>
+            <div class="order-status">
+                <div v-if="order.status == 0" class="flex">
+                    <span class="status-dot yellow-dot"></span>
+                    <div class="status-text">{{ $t('profile>order.pending') }}</div>
+                </div>
+                <div v-if="order.status == 1 && isServiceInProgressState(order)" class="flex">
+                    <span class="status-dot green-dot"></span>
+                    <div class="status-text">{{ $t('profile>order.processing') }}</div>
+                </div>
+                <div v-if="order.status == 1 && !isServiceInProgressState(order)" class="flex">
+                    <span class="status-dot green-dot"></span>
+                    <div class="status-text">{{ $t('profile>order.confirmed') }}</div>
+                </div>
+                <div v-if="order.status == 2" class="flex">
+                    <span class="status-dot gray-dot"></span>
+                    <div class="status-text">{{ $t('profile>order.completed') }}</div>
+                </div>
+                <div v-if="order.status == 3" class="flex">
+                    <div class="status-dot red-dot"></div>
+                    <div class="status-text">{{ $t('profile>order.canceled') }}</div>
                 </div>
             </div>
         </div>
@@ -71,23 +71,37 @@
 
 
 <script>
-import orderDetail from './order-detail/order-detail.vue';
+
+import $common from "../../utils/common";
 
 export default {
-    name:"order",
+    name: "order",
+    computed: {
+        $common() {
+            return $common
+        }
+    },
     data() {
         return {
             orderList: [],
             searchKeyword: '',
+            serviceTypeMap: {},
+            addressMap: {},
         };
     },
     onShow() {
-        this.reload()
+        this.reload();
     },
     methods: {
-        reload() {
+        async reload() {
             this.resetPagination();
+            await this.getServantTypeList();
             this.getDataList();
+        },
+        resetPagination() {
+            this.page = 1;
+            this.hasMore = true;
+            this.orderList = [];
         },
         buildApiParams() {
             let url = getApp().globalData.data.requestUrl + this.$API.order.search;
@@ -97,28 +111,16 @@ export default {
                 page: this.page,
                 size: this.pageSize,
             };
-            let data = {};
-
-            if(this.searchKeyword && this.searchKeyword.trim() !== '') {
-                data = {
-                    ...baseData,
-                    keyword: this.searchKeyword,
-                };
-            } else {
-                data = {
-                    ...baseData,
-                };
-            }
+            let data = this.searchKeyword.trim() !== '' ? {...baseData, keyword: this.searchKeyword} : baseData;
             return {url, method, data};
         },
         onSearchInput() {
             this.reload();
         },
-        getDataList() {
-            if (this.loading || !this.hasMore||this.$common.isEmpty(uni.getStorageSync(getApp().globalData.data.userInfoKey).id)) return;
+        async getDataList() {
+            if(this.loading || !this.hasMore || this.$common.isEmpty(uni.getStorageSync(getApp().globalData.data.userInfoKey).id)) return;
 
             this.loading = true;
-
             const {url, method, data} = this.buildApiParams();
 
             uni.request({
@@ -127,19 +129,30 @@ export default {
                 data: data,
                 success: (res) => {
                     const orders = res.data.list;
-                    if (this.page === 1) {
-                        this.orderList = [];
-                    }
-                    if (orders.length < this.pageSize) {
+                    if(this.page === 1) this.orderList = [];
+
+                    // Check if there are more pages
+                    if(orders.length < this.pageSize) {
                         this.hasMore = false;
                     }
-                    // Append new orders to the list
-                    this.orderList = this.orderList.concat(orders);
-                    // Process 'createdAt' fields
-                    this.orderList.forEach((order) => {
+
+                    // Append new orders to the list and map servant type names
+                    orders.forEach((order) => {
+                        // Convert timestamp to readable time
                         order.createdAt = order.createdAt ? this.$common.stampToTime(order.createdAt) : '';
+
+                        // Add service type name and nameCn to the order
+                        if(this.serviceTypeMap[order.requiredServantType]) {
+                            order.serviceTypeName = this.serviceTypeMap[order.requiredServantType].name;
+                            order.serviceTypeNameCn = this.serviceTypeMap[order.requiredServantType].nameCn;
+                        }
+
                     });
 
+                    // Update the order list
+                    this.orderList = this.orderList.concat(orders);
+
+                    // Increment the page number for the next call
                     this.page += 1;
                 },
                 complete: () => {
@@ -147,21 +160,38 @@ export default {
                 },
             });
         },
+        async getServantTypeList() {
+            return new Promise((resolve, reject) => {
+                uni.request({
+                    url: getApp().globalData.data.requestUrl + this.$API.serviceType.search,
+                    method: 'POST',
+                    data: {},
+                    success: (res) => {
+                        res.data.list.forEach((serviceType) => {
+                            // Populate the serviceTypeMap
+                            this.serviceTypeMap[serviceType.id] = {
+                                name: serviceType.name,
+                                nameCn: serviceType.nameCn,
+                            };
+                        });
+                        resolve(); // Resolve the promise after fetching the data
+                    },
+                    fail: (error) => {
+                        console.error("Failed to fetch servant type list:", error);
+                        reject(error); // Reject the promise in case of failure
+                    },
+                });
+            });
+        },
         isServiceInProgressState(order) {
             if(order.serviceScheduleStart && order.serviceScheduleEnd) {
                 const currentTime = new Date().getTime();
                 const serviceStartTime = new Date(order.serviceScheduleStart).getTime();
                 const serviceEndTime = new Date(order.serviceScheduleEnd).getTime();
-                if(currentTime >= serviceStartTime && currentTime <= serviceEndTime) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
+                return currentTime >= serviceStartTime && currentTime <= serviceEndTime;
             }
+            return false;
         },
-
         // Redirects
         orderDetailRedirect(orderId) {
             uni.navigateTo({
@@ -172,21 +202,46 @@ export default {
 };
 </script>
 
-<style scoped>
-.order-content {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-}
 
+<style scoped>
 .order-icon {
     width: 40px;
     height: 40px;
     margin-right: 10px;
 }
 
-.order-detail {
+.order-schedule {
+    width: 100%;
+    padding: 2px 8px;
+    margin: 0 0 8px 0;
+    background-color: white;
+    border-radius: 50px;
+    justify-content: space-between;
+    display: flex;
+}
+
+.service-type-price {
+    width: 100%;
+    display: flex;
+    justify-content: space-between
+}
+
+.order-address{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: #2676f7;
+    border-radius: 10px;
+    padding: 6px;
+    color: white;
+    font-weight: bold;
+    margin: 8px 0;
+}
+
+.order-status {
     margin-top: 5px;
+    display: flex;
+    align-self: flex-end;
 }
 
 .status-dot {
@@ -201,7 +256,6 @@ export default {
     position: relative;
     bottom: 4px;
     left: 5px;
-    margin-right: 12px;
 }
 
 .yellow-dot {
@@ -218,10 +272,6 @@ export default {
 
 .gray-dot {
     background-color: #8c8c8c;
-}
-
-.order-status {
-    margin-right: 10px;
 }
 
 .highlight-blue {
