ac8596ee2a9e49c80b0b56ed9ccea6f4dff96792
Percival <emperorsheevpalpatine0@gmail.com>
2024-10-24 06:07:41 +0800

    config(BE): SSL certificate configuration

--- linkup-server/src/main/java/com/enchanted/constant/WeChatConstant.java
+++ linkup-server/src/main/java/com/enchanted/constant/WeChatConstant.java
@@ -3,7 +3,7 @@ package com.enchanted.constant;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class WeChatConstant {
-    private static final Dotenv dotenv = Dotenv.load();
+    private static final Dotenv dotenv = Dotenv.configure().filename(".env").load();
     public static final String ACCESS_TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token";
     public static final String QR_CODE_Url = "https://api.weixin.qq.com/wxa/getwxacode";
     public static final String APP_ID = dotenv.get("WECHAT_APP_ID");
